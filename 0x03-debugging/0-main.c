9f0a9d7b2dc5c3f6b5222e934f9cc499d3f031c4
SamAyo <amoranonaopemipo@gmail.com>
2023-07-30 10:52:16 -0700

    Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* main - This file must test that the function positive_or_negative() gives the correct output when given a case
+* Return: 0
+*/
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
+}
